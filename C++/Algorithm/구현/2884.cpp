1c7be8c84c02dd1c33de58dbaa09452e52fe8238
haruSP <jeongseok0019@gmail.com>
2023-06-13 17:23:18 +0900

    add: c++ 백준 2884

---
+++ C++/Algorithm/구현/2884.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int hour, min; // ½Ã, ºÐ 
+	cin >> hour >> min;
+	if (min < 45) {
+		min = min + 15;
+		hour = hour - 1;
+		if (hour < 0) {
+			hour = 23;
+		}
+	} else {
+		min = min - 45;
+	}
+	cout << hour << ' ' << min << '\n';
+}
