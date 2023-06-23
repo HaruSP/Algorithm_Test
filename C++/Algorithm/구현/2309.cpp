251c0ed4cad214297e10332bb11b8ad3b99b86f6
haruSP <jeongseok0019@gmail.com>
2023-06-23 16:29:31 +0900

    add: 백준 2309 풀이

---
+++ C++/Algorithm/구현/2309.cpp
@@ -0,0 +1,100 @@
+
+// 순열 
+#include<bits/stdc++.h>
+using namespace std;
+int a[9];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	for(int i = 0; i < 9; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + 9);
+	do{
+		for(int i : a)cout << i << " ";
+		cout << '\n';
+		int sum = 0;
+		for(int i = 0; i < 7; i++) sum += a[i];
+		
+		if(sum == 100)break;
+	}while(next_permutation(a, a + 9));
+	for(int i = 0; i < 7; i++) cout << a[i] << '\n';
+	return 0;
+}
+
+// 조합(중첩for문) 
+#include<bits/stdc++.h>
+using namespace std;
+int a[9], sum;
+vector<int> v;
+pair<int, int> ret;
+void solve() {
+	for(int i = 0; i < 9; i++) {
+		for(int j = 0; j < i; j++) {
+			if(sum - a[i] -a[j] == 100) {
+				ret = {i, j};
+				return;
+			}
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	for(int i = 0; i < 9; i++) {
+		cin >> a[i]; sum += a[i];
+	}
+	solve();
+	for(int i = 0; i < 9; i++) {
+		if(ret.first == i || ret.second == i) continue;
+		v.push_back(a[i]);
+	}
+	sort(v.begin(), v.end());
+	for(int i : v) cout << i << " ";
+//	for(int i = 0; i < v.size(); i++) {
+//		cout << v[i] << " ";
+//	}
+	return 0;
+}
+
+// 재귀함수 순열 
+#include<bits/stdc++.h>
+using namespace std;
+int a[9];
+int n = 9, r = 7;
+void solve() {
+	int sum = 0;
+	for(int i = 0; i < r; i++) {
+		sum += a[i];
+	}
+	if(sum == 100) {
+		sort(a, a + 7);
+		for(int i = 0; i < r; i++) cout << a[i] << '\n';
+		exit(0); // 전체함수 종료 
+	}
+}
+void print() {
+	for(int i = 0; i < r; i++) cout << a[i] << " ";
+	cout << '\n';
+}
+void makePermutation(int n, int r, int depth) {
+	if(r == depth) {
+		solve();
+		return;
+	}
+	for(int i = depth; i < n; i++) {
+		swap(a[i], a[depth]);
+		makePermutation(n, r, depth + 1);
+		swap(a[i], a[depth]);
+	}
+	return;
+}
+int main() {
+	for(int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	makePermutation(n, r, 0);
+	return 0;
+}
