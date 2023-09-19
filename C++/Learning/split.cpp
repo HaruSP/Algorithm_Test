41d2b8f62d292b15bca4a72054d78237f8040949
haruSP <jeongseok0019@gmail.com>
2023-09-19 17:36:06 +0900

    fix: c++ split 수정

--- C++/Learning/split.cpp
+++ C++/Learning/split.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+void init() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
 
 vector<string> split(string input, string delimiter) {
 	vector<string> ret;
 	long long pos = 0;
 	string token = "";
-	while ((pos = input.find(delimiter)) != string::npos) {
+	while ( (pos = input.find(delimiter)) != string::npos )
+	{
 		token = input.substr(0, pos);
 		ret.push_back(token);
 		input.erase(0, pos + delimiter.length());
@@ -13,10 +19,10 @@ vector<string> split(string input, string delimiter) {
 	ret.push_back(input);
 	return ret;
 }
-
 int main() {
-	string s = "abcdfffasdaffasdgdsadffsdadsw", d = "ff";
+	init();
+	string s = "배가 많이 고프시지 않나요?", d = " ";
 	vector<string> a = split(s, d);
-	for(string b : a) cout << b << '\n';
+	for ( string b : a ) 
+		cout << b << '\n';
 }
-
